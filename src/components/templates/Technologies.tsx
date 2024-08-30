5eddf2473c9cfebbcb8f7bb23a4d358e8cc1164f
ekorahy <ekorahy@gmail.com>
2024-08-30 23:20:09 +0700

    feat(technologies): add contents

---
+++ src/components/templates/Technologies.tsx
@@ -0,0 +1,12 @@
+import TechList from "../organisms/TechList";
+
+export default function Technologies() {
+  return (
+    <section>
+      <h2 className="mb-8 text-center text-3xl font-bold lg:text-4xl">
+        Technologies
+      </h2>
+      <TechList />
+    </section>
+  );
+}

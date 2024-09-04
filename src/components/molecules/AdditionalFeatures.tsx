8278c799979933969f096eeea91779cd6d754275
ekorahy <ekorahy@gmail.com>
2024-09-05 06:48:23 +0700

    feat: add dark mode feature

--- src/components/molecules/AdditionalFeatures.tsx
+++ src/components/molecules/AdditionalFeatures.tsx
@@ -1,11 +1,12 @@
-import ButtonDarkMode from '../atoms/ButtonDarkMode';
+import ButtonDarkMode from '../atoms/ToggleDarkMode';
 import ButtonLanguages from '../atoms/ButtonLanguages';
+import ToggleDarkMode from '../atoms/ToggleDarkMode';
 
 export default function AdditionalFeatures() {
   return (
     <div className='absolute bottom-8 flex gap-4 lg:relative lg:bottom-auto'>
       <ButtonLanguages />
-      <ButtonDarkMode />
+      <ToggleDarkMode />
     </div>
   );
 }

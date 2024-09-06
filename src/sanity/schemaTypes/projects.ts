b6aab1cce802e85562a2acd48dbe1fabb65ff3b7
ekorahy <ekorahy@gmail.com>
2024-09-06 13:07:55 +0700

    feat(porjects): add description field in projects schema

--- src/sanity/schemaTypes/projects.ts
+++ src/sanity/schemaTypes/projects.ts
@@ -34,6 +34,11 @@ export default defineType({
       },
     }),
     defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'string'
+    }),
+    defineField({
       name: 'created_at',
       title: 'Created At',
       type: 'datetime',

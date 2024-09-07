750ee232dfb7cfd77b3f64a440b832e415e31c57
ekorahy <ekorahy@gmail.com>
2024-09-07 23:40:52 +0700

    feat(blog): change max data for other blog to 3 and add feature to shuffle the data

--- src/utils/otherBlog.ts
+++ src/utils/otherBlog.ts
@@ -1,7 +1,7 @@
 import { BlogProps } from "@/types/blog";
 
 export function getOtherBlog(blog: BlogProps[], title: string) {
-  return blog
-    .filter(blog => blog.title !== title)
-    .slice(0, 6);
+  const filteredBlogs = blog.filter(blog => blog.title !== title);
+  const shuffledBlogs = filteredBlogs.sort(() => Math.random() - 0.5);
+  return shuffledBlogs.slice(0, 3);
 }
